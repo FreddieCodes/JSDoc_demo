44554514dd91e6e62a4470e3ebc297e42cda99b5
Frederick Ofori <FrederickCodes@gmail.com>
2018-01-10 11:15:54 +0000

    MIT-2090 - added person.js and jsdoc comments

---
+++ src/person/person.js
@@ -0,0 +1,50 @@
+/** Class representing a person. */
+export class Person {
+  /**
+   * Create a Person.
+   * @param {string} name Name of the person.
+   * @param {int} yearOfBirth Year of birth.
+   * @param {string} [job=false] - The person's job.
+   */
+  constructor(name, yearOfBirth, job = false) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+
+  /**
+   * Get the person's name
+   * @returns {string}
+   */
+  getName() {
+    return this.name;
+  }
+
+  /**
+   * Changes the person's job title
+   * @param {string} newJob New job title.
+   */
+  changeJob(newJob) {
+    this.job = newJob;
+  }
+
+  /**
+   * Get the person's job
+   * @returns {string} Job status.
+   */
+  getJob() {
+    if (this.job === false) {
+      return `${this.name} is unemployed.`;
+    }
+    return `${this.name} works as a ${this.job}.`;
+  }
+
+  /**
+   * Calculate the person's approx age
+   * @returns {number} Age in years.
+   */
+  calculateApproxAge() {
+    const age = new Date().getFullYear() - this.yearOfBirth;
+    return age;
+  }
+}
